2f9802759ec17641668a2a3e4f6be60e1ec44b3d
mariacarilinass <m.carolina.soares1@gmail.com>
2024-07-31 10:29:37 -0300

    criei um utils do bd para carregar a query

---
+++ app/util/models.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python# -*- coding: utf-8 -*-
+
+
+class Query():
+    def search_paginate(model, search_term, page, per_page=12):
+        return model.query.filter(model.title.like(f'%{search_term}%')).order_by(
+            model.timestamp.desc()).paginate(page=page, per_page=per_page)
