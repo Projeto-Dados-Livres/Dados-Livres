3ac8a4ff6f4bb0266ecffe79e20b8690e16c8244
mariacarilinass <m.carolina.soares1@gmail.com>
2024-08-05 13:50:07 -0300

    add atributo de erro das páginas

--- app/util/models.py
+++ app/util/models.py
@@ -4,4 +4,4 @@
 class Query():
     def search_paginate(model, search_term, page, per_page=12):
         return model.query.filter(model.title.like(f'%{search_term}%')).order_by(
-            model.timestamp.desc()).paginate(page=page, per_page=per_page)
+            model.timestamp.desc()).paginate(page=page, per_page=per_page, error_out=False)
